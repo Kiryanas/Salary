c9ca135c89873410e8cfd17bf4ed6377b3a93db8
daniil-kiryanov <daniil.kiryanov@gmail.com>
2022-08-14 22:48:22 +0400

    работает на флешке

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
-﻿
-using System.IO;
+﻿using System.IO;
 using System.Collections.Generic;
 
 public class Reader
 {
     static void Main()
     {
-        File.WriteAllText(@"SampleOutput.txt", string.Empty);
+        File.WriteAllText(@"D:\SampleOutput.txt", string.Empty);
         var InputTable = new Dictionary<string, string>();
         var OutputTable = new Dictionary<string, string>();
         Console.Write("Введите размер премии: ");
         int premium = int.Parse(Console.ReadLine());
-        foreach (string line in File.ReadLines(@"D:\\SampleInput.txt"))
+        foreach (string line in File.ReadLines(@"D:\SampleInput.txt"))
         {
             string[] subs = line.Split('\t');
             InputTable.Add(subs[0], subs[1]);
@@ -38,7 +37,7 @@ public class Writer
 {
     public static async Task FileWrtr(string str)
     {
-        using StreamWriter file = new("SampleOutput.txt", append: true);
+        using StreamWriter file = new("D:\\SampleOutput.txt", append: true);
         await file.WriteLineAsync(str);
     }
 }
